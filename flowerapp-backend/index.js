c80d8ae99e0ac99ec0352947423a046e3f7ead5a
Rashid Ahmad <decent10cs@gmail.com>
2020-06-15 22:16:30 +0500

    added recaptcha to the flowerapp-front app

--- flowerapp-backend/index.js
+++ flowerapp-backend/index.js
@@ -1,15 +1,22 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
-
-
+const request = require('request');
+const bodyParser = require('body-parser');
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 const uuid = require("uuid/v4");
 
 const app = express();
 
+const Recaptcha = require('recaptcha-verify');
+const recaptcha = new Recaptcha({
+    secret: '6Lez_6IZAAAAAEL1gr1pZQlDjRTQ-ZOZDURNyY8B',
+    verbose: true
+});
 //middleware
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 //routes
@@ -49,6 +56,51 @@ app.get("/", (req, res) => {
 //     .then(result => res.status(200).json(result))
 //     .catch(err => console.log(err));
 // });
+app.post('/verify',  (req, res) => {
+
+
+  recaptcha.checkResponse(req.body.captcha, function(error, response){
+       if(error){
+           // an internal error?
+          res.json({ succuess: false,response });
+           return;
+       }
+       if(response.success){
+             res.json({ succuess: true,response });
+           // save session.. create user.. save form data.. render page, return json.. etc.
+       }else{
+            res.json({ succuess: false ,response });
+           // show warning, render page, return a json, etc.
+       }
+     })
+//   if (!req.body.captcha) return res.json({ success: false, msg: 'Please select captcha' });
+//
+//   // Verify URL
+//   const query = JSON.stringify({
+//     secret: secretKey,
+//     response: req.body.captcha,
+//     remoteip: req.connection.remoteAddress
+//   });
+//   const verifyURL = `https://google.com/recaptcha/api/siteverify?${query}`;
+//
+//   // Make a request to verifyURL
+// console.log(query);
+//     request({url:verifyURL,method:"POST", headers: { "Content-Type": "application/x-www-form-urlencoded" }}, function(err, response, body) {
+//       body = JSON.parse(body);
+//       console.log(body);
+//             // Success will be true or false depending upon captcha validation.
+//                 if(body.success !== undefined && !body.success) {
+//                     res.send({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
+//                 }else{
+//                     res.send({"responseCode" : 0,"responseDesc" : "Sucess"});
+//                 }
+//
+//
+//
+//
+//
+//   });
+})
 app.post("/checkout", async (req, res) => {
   console.log("Request:", req.body);
 
