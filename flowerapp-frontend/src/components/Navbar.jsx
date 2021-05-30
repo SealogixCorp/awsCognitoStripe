00466648ffbb0999ec9e66139bbea1eefe3c4964
Rashid Ahmad <decent10cs@gmail.com>
2021-05-30 20:28:50 +0500

    make link from the logo

--- flowerapp-frontend/src/components/Navbar.jsx
+++ flowerapp-frontend/src/components/Navbar.jsx
@@ -97,7 +97,9 @@ const Navbar = ({ backgroundColor = "bg-gray-900" }) => {
           <div className="flex-shrink-0 flex items-center">
             {/* <img className="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
                   <img className="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" /> */}
+                  <a href="/" className="   px-3 py-2 rounded-md text-sm font-medium">
              <img className="block h-8 w-auto" src="logo.png" alt="logo"  />
+             </a>
           </div>
         </div>
         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 text-xs ">
