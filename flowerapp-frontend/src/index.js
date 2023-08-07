eacd0ac04d8d03c17d52ac6ae799fb4f534f0a80
Rashid Ahmad <decent10cs@gmail.com>
2023-08-07 23:17:52 +0200

    update packages to latest version

--- flowerapp-frontend/src/index.js
+++ flowerapp-frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 
 
-import Amplify from "aws-amplify";
+import {Amplify} from "aws-amplify";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ToastProvider } from "react-toast-notifications";
 import "./index.css";
